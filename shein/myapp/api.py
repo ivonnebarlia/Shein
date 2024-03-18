c2a85e64897228d9dae0a2300550716b729a391f
Ivonne Barlia <ivonnebarlia@gmail.com>
2024-03-17 22:49:21 -0400

    serializer

--- shein/myapp/api.py
+++ shein/myapp/api.py
@@ -1,8 +1,15 @@
-from .models import Product
+from .models import Product, Transaction
 from rest_framework import viewsets, permissions
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, TransactionSerializer
+
 
 class ProductViewSet(viewsets.modelViewSet):
     queryset = Product.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = ProductSerializer
+
+
+class TransactionViewSet(viewsets.modelViewSet):
+    queryset = Transaction.objects.all()
+    permissions_classes = [permissions.AllowAny]
+    serializer_class = TransactionSerializer
--- shein/myapp/serializers.py
+++ shein/myapp/serializers.py
@@ -1,14 +1,33 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Transaction
 
 
 class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ('name', 'description', 'sales_price')
+        fields = ('id', 'name', 'description', 'sales_price')
 
-    name =serializers.CharField(max_length=100)
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Product.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    product_id = ProductSerializer(read_only=True)
+    class Meta:
+        model_transaction = Transaction
+        fields = ('id', 'product_id', 'amount', 'price', 'date', 'type_transaction', 'suppler ')
 
     def create(self, validated_data):
         """

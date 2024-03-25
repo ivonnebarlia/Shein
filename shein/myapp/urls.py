c4807222256b51b377e8522074b58f30a65e5c63
Ivonne Barlia <ivonnebarlia@gmail.com>
2024-03-24 22:58:11 -0400

    serializer

--- shein/myapp/urls.py
+++ shein/myapp/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework import routers
-from shein.myapp.viewsets.ProductViewset import ProductViewSet
-
+from shein.myapp.viewsets import ProductViewSet
 
 router = routers.DefaultRouter()
 
-router.register('api/products', ProductViewSet, 'products')
+router.register('api/Product', ProductViewSet, 'Product')
 
 router = routers.SimpleRouter()
 router.register(r'Product', ProductViewSet, basename='Product')
 
 urlpatterns = [
      path("", views.index, name="index"),
+     path('', include(router.urls)),
 ]

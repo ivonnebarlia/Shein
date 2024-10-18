ccf090bc943f91dbe72a2378ddbeb81ba3c8824e
Jose Barlia <ibarlia@gmail.com>
2024-10-18 17:10:21 +0300

    fixing conflicts

--- shein/myapp/models.py
+++ shein/myapp/models.py
@@ -8,18 +8,8 @@ class Product(models.Model):
     description = models.TextField()
     sale_price = models.DecimalField(max_digits=10, decimal_places=2)
 
-
-class Transaction(models.Model):
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    amount = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
-    type_transaction = models.CharField(max_length=50)
-    suppler = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
-
 
 
 class Transaction(models.Model):
@@ -31,8 +21,7 @@ class Transaction(models.Model):
     suppler = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
-
+        return self.product_id
 
     class Meta:
         ordering = ('id',)
--- shein/myapp/urls.py
+++ shein/myapp/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from . import views
-from rest_framework import routers
 from myapp.viewsets import ProductViewSet
 from rest_framework.routers import DefaultRouter
 from .viewsets.TransactionViewset import TransactionViewSet
@@ -8,8 +6,8 @@ from .viewsets.TransactionViewset import TransactionViewSet
 router = DefaultRouter()
 router.register('api/products', ProductViewSet, 'products')
 router.register(r'transactions', TransactionViewSet, basename='transactions')
-urlpatterns = [
-     path("", views.index, name="index"),
+# urlpatterns = [
+#      path("", views.index, name="index"),
 urlpatterns = [
      path('', include(router.urls)),
 ]

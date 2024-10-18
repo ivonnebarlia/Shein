36ea3b46fa55265299289e79c796006c8bef410a
Jose <41075078+jibarlia@users.noreply.github.com>
2024-10-18 17:23:05 +0300

    Serializer 1 (#7)
    adding serializer
    
    ---------
    
    Co-authored-by: Ivonne Barlia <ivonnebarlia@gmail.com>

--- shein/myapp/models.py
+++ shein/myapp/models.py
@@ -8,6 +8,15 @@ class Product(models.Model):
     description = models.TextField()
     sale_price = models.DecimalField(max_digits=10, decimal_places=2)
 
+
+class Transaction(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    type_transaction = models.CharField(max_length=50)
+    suppler = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
